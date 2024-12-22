f9de029a49355f2be5359897cbf6a82cc314c0eb
Chandru J <chandrukavin0503@gmail.com>
2024-12-22 20:07:09 +0530

    Home Page Done.

---
+++ components/scripts/head.js
@@ -0,0 +1,45 @@
+// 1. ITA Title
+
+$(document).ready(function() {
+    $(".ITA-text").lettering('words').children("span").lettering(); 
+})
+
+// 2. INFORMATION TECHNOLOGY ASSOCIATION
+
+function _toArray(arr) { 
+    return Array.isArray(arr) ? arr : Array.from(arr);
+}
+
+function makeSpans(selector) {
+    var elements = _toArray(document.querySelectorAll(selector));
+
+    return elements.map(function (element) {
+        var text = element.innerText.split('');
+        var spans = text.map(function (letter) {
+        return '<span>' + letter + '</span>';
+        }).join('');
+        return element.innerHTML = spans;
+    });
+}
+
+makeSpans('.main-transition');
+
+// 3. ITA DESCRIPTION
+
+function _toArray(arr) { 
+    return Array.isArray(arr) ? arr : Array.from(arr);
+  }
+  
+  function makeDescriptionSpans(selector) {
+    var elements = _toArray(document.querySelectorAll(selector));
+  
+    return elements.map(function (element) {
+      var text = element.innerText.split(' ');
+      var spans = text.map(function (letter) {
+        return '<span>' + letter + '</span>';
+      }).join(' ');
+      return element.innerHTML = spans;
+    });
+  }
+  
+  makeDescriptionSpans('.description-transition');
